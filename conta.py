417e6e0aa0ce2166d317c1dbf8b6333dc9d4cdde
Leonardo Lima <leonardolimarj@outlook.com>
2023-02-22 16:53:17 -0300

    seguindo
    Encapsulamento

--- conta.py
+++ conta.py
@@ -3,10 +3,10 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("COnstruindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
         print("Saldo {} do Titular {}".format(self.saldo, self.titular))
@@ -16,3 +16,21 @@ class Conta:
 
     def saca(self, valor):
         self.saldo -= valor
+
+    def transefere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+    @property
+    def limite(self):
+        return self.__limite
+
+
